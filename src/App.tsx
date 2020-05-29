0bcc234af425f3f8fbe1fe64c0cd8dc06dfa4ac6
Robert Starbuck <robert.starbuck@polecat.com>
2020-05-30 00:28:18 +0100

    Fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
 import './App.css';
 
-import {encode, decode} from 'z-chars';
+import { encode, decode } from 'z-chars';
 
 
 function App() {
-  
+
   const [mode, setMode] = useState<"encode" | "decode">("encode");
 
   const [subject, setSubject] = useState("");
@@ -14,37 +14,46 @@ function App() {
 
   const [toCopy, setToCopy] = useState("");
 
-  const onModeToggle = () => setMode(m => m === "encode" ? "decode" : "encode")
+  const onToggleMode = () => setMode(m => m === "encode" ? "decode" : "encode")
+
+  const onEncodeMode = () => setMode("encode");
+  const onDecodeMode = () => setMode("decode");
 
   const onSubjectUpdate = (e: ChangeEvent<HTMLTextAreaElement>) => setSubject(e.currentTarget.value);
   const onEncodeUpdate = (e: ChangeEvent<HTMLTextAreaElement>) => setToEncode(e.currentTarget.value);
   const onDecodeUpdate = (e: ChangeEvent<HTMLTextAreaElement>) => setToDecode(e.currentTarget.value);
 
   useEffect(() => {
-    if(mode ==="encode"){
+    if (mode === "encode") {
       setToCopy(encode(subject.trim(), toEncode.trim()));
     }
-    if(mode ==="decode"){
+    if (mode === "decode") {
       setToCopy(decode(toDecode));
     }
   }, [mode, subject, toEncode, toDecode])
 
-
   return (
     <div className="Wall">
+
+      <button onClick={onEncodeMode}>← Encode</button>
+      <button onClick={onDecodeMode}>Decode →</button>
+
       <div className="App">
-        <form className={mode}>
-          <textarea className="encode" onChange={onSubjectUpdate} value={subject} />
-          <textarea className="encode" onChange={onEncodeUpdate} value={toEncode} />
-
-          <textarea value={toCopy} />
-
-          <textarea className="decode" onChange={onDecodeUpdate} value={toDecode} />
+        <form className={mode} onSubmit={e => e.preventDefault()}>
+          <div>
+            <textarea className="encode" onChange={onSubjectUpdate} value={subject} />
+          </div>
+          <div>
+            <textarea className="encode" onChange={onEncodeUpdate} value={toEncode} />
+          </div>
+          <div>
+            <textarea className="copy" value={toCopy} />
+            <button>Copy</button>
+          </div>
+          <div>
+            <textarea className="decode" onChange={onDecodeUpdate} value={toDecode} />
+          </div>
         </form>
-
-        <footer>
-          <code onClick={onModeToggle}>{mode === "encode" ? "ENCODE" : "DECODE"}</code>
-        </footer>
       </div>
     </div>
   );
